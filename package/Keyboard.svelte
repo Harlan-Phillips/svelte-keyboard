129436a92b628aab2a59fd1417a8048ca67c55ba
Harlan <harlanphillips74@gmail.com>
2025-10-02 22:06:03 -0700

    Fix double-height button width for grid layout

--- package/Keyboard.svelte
+++ package/Keyboard.svelte
@@ -146,10 +146,9 @@
     position: absolute;
     right: 0;
     top: 0;
-    height: calc(200% + var(--margin, 0.125rem) * 4);
+    height: calc(200% + 0.8rem);
     z-index: 10;
-    flex: 0 0 auto;
-    width: calc(var(--height, 3.5rem) + var(--margin, 0.125rem) * 2);
+    width: calc((100% - 3 * 0.8rem) / 4);
   }
 
   button {
